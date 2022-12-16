78662920c3f1ede2c31fc7f978e5d9eb4e2ace7b
bokycodes <114671954+bokycodes@users.noreply.github.com>
2022-12-16 15:31:44 +0100

    Made function game() that plays 5 rounds of rock paper scissors and displays the winner at the end!

--- script.js
+++ script.js
@@ -19,30 +19,58 @@ let playRound = (playerSelection, computerSelection) => {
 
     //Player and Computer both select the same weapon
     if(playerSelection === computerSelection){
-        return "No winner!";
+        console.log("No winner in this round!");
+        return;
     }
     
     //Player selects rock
     if(playerSelection === "rock"){
-        if(computerSelection === "paper") return "Computer wins!";
-        else if(computerSelection === "scissors") return "Player wins!";
+        if(computerSelection === "paper") return 0;
+        else if(computerSelection === "scissors") return 1;
     }
 
     //Player selects paper
     if(playerSelection === "paper"){
-        if(computerSelection === "scissors") return "Computer wins!";
-        else if(computerSelection === "rock") return "Player wins!";
+        if(computerSelection === "scissors") return 0;
+        else if(computerSelection === "rock") return 1;
     }
 
     //Player selects scissors
     if(playerSelection === "scissors"){
-        if(computerSelection === "rock") return "Computer wins!";
-        else if(computerSelection === "paper") return "Player wins!";
+        if(computerSelection === "rock") return 0;
+        else if(computerSelection === "paper") return 1;
     }
-
 
 };
 
-let computerSelection = getComputerChoice().toLowerCase();
-let playerSelection = prompt("Select your weapon (rock, paper or scissors): ").toLowerCase();
-playRound(playerSelection, computerSelection);
+let game = function(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for(i = 0; i < 5; i++) {
+
+        let playerSelection = prompt("Select your weapon (rock, paper or scissors): ").toLowerCase();
+        let computerSelection = getComputerChoice().toLowerCase();
+
+        if(playRound(playerSelection, computerSelection)){ // equal to playRound(...) === 1
+            playerScore++;
+            console.log("Player wins this round!");
+            console.log("Player score: "+playerScore +"\n" + "Computer score: "+computerScore);
+        }
+        else{
+            computerScore++;
+            console.log("Computer wins this round!");
+            console.log("Player score: "+playerScore +"\n" + "Computer score: "+computerScore);
+        }
+    }
+
+    if(playerScore > computerScore){
+        console.log("PLAYER WINS, GOOD GAME!");
+    }
+    else{
+        console.log("COMPUTER WINS, GOOD GAME!");
+    }
+};
+
+game();
